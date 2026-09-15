943c850161f736f30bc51d250f3aed441c0380af
abhishekranjansrivastava19 <abhishekranjansrivastava19@gmail.com>
2026-09-15 14:31:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3011,7 +3011,7 @@ app.get("/exam-calendar/:school_code", async (req, res) => {
     try {
         const { school_code } = req.params;
 
-        console.log(req.body)
+        console.log(req.params)
 
         if (!school_code) {
             return res.status(400).json({
